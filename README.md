058884404f21696abd0e6b00e71146cfd9fd7ca0
swanyee <swanyeetun.2000@gmail.com>
2022-03-06 19:28:02 +0630

    final stage

---
+++ README.md
@@ -0,0 +1,3 @@
+## Challenge from Frontend Master
+
+You can check many frontend design challenge from there
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      "light-grey": "hsl(212, 45%, 89%)",
+      "grey-blue": "hsl(220, 15%, 55%)",
+      "dark-blue": "hsl(218, 44%, 22%)",
+    },
+    fontFamily: {
+      Outfit: ["Outfit", "sans-serif"],
+    },
+    fontSize: {
+      para: "15px",
+      title: "20px",
+    },
+  },
+  plugins: [],
+};
